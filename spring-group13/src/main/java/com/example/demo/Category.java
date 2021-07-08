b3fd6565d535d163d6f65126298494b44711ae7e
id <id@IN0013402>
2021-07-08 13:05:39 +0900

    7/8 2nd

---
+++ spring-group13/src/main/java/com/example/demo/Category.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int code;
+	private String name;
+
+	Category(){
+
+	}
+
+	public Category(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	}
