ee6c295226801cc4dce48b1c34135803be695ac7
id <id@IN0013402>
2021-07-09 13:06:06 +0900

    7/9 2nd

--- spring-group13/src/main/java/com/example/demo/ScheduleController.java
+++ spring-group13/src/main/java/com/example/demo/ScheduleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +16,19 @@ public class ScheduleController {
 	private ScheduleRepository scheduleRepository;
 
 	@RequestMapping("/fill")
+	public ModelAndView fill(ModelAndView mv) {
+
+		List<Schedule>list = scheduleRepository.findAll();
+
+
+		mv.addObject("list",list);
+
+		mv.setViewName("fillout");
+
+		return mv;
+	}
+
+	@PostMapping("/fill")
 	public ModelAndView schedule(
 			@RequestParam("name") String name,
 			@RequestParam("jikan") Time jikan,
