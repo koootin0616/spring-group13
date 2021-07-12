6914a59c7f16d1c6eb57bfdc387744df8d280220
id <id@IN0013402>
2021-07-12 14:08:28 +0900

    7/12 2.1

--- spring-group13/src/main/java/com/example/demo/CategoryContoroller.java
+++ spring-group13/src/main/java/com/example/demo/CategoryContoroller.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class CategoryContoroller {
 	@Autowired
@@ -54,5 +53,4 @@ public class CategoryContoroller {
 
 		return mv;
 	}
-
 }
