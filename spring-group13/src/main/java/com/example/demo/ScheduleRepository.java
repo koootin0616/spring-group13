41eeeba006ffa64a321f2abb8b6cc0fe580651b2
id <id@IN0013402>
2021-07-14 13:06:29 +0900

    7/14 1st

--- spring-group13/src/main/java/com/example/demo/ScheduleRepository.java
+++ spring-group13/src/main/java/com/example/demo/ScheduleRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule,Integer> {
 	List<Schedule> findByCategorycode(int categorycode);
+	List<Schedule> findByUsercodeAndYmd(int usercode,Date ymd);
 }
